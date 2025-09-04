3245953f1afda3e76548ade4d6cb4112846744d0
Sajilaa <sajilaa29122003@gamil.com>
2025-09-04 17:49:32 +0530

    printdate

---
+++ src/java_8_versions_Learning/datetime/printdate.java
@@ -0,0 +1,11 @@
+package java_8_versions_Learning.datetime;
+
+import java.time.LocalDate;
+
+public class printdate {
+    public static void main(String[] args)
+    {
+       LocalDate res= LocalDate.now();
+        System.out.println(res);
+    }
+}
